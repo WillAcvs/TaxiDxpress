694627c5237443d1f55fb0507ee81378fab5656f
WillAcvs <desarrollointegralapp@gmail.com>
2021-01-07 22:47:02 -0700

    upload project

---
+++ config/mainconfig.js
@@ -0,0 +1,21 @@
+export const MainConfig = {
+    AppDetails: {
+        app_name: "GrabCab", 
+        app_description: "GrabCab Taxi Booking App",
+        app_identifier: "com.exicube.grabcab",
+        ios_app_version: "2.1", 
+        android_app_version: 21
+    },
+    FirebaseConfig: {
+        apiKey: "XXXXXXXXXXXXXXXXX",
+        authDomain: "XXXXXXXXXXXXXXXXX",
+        databaseURL: "XXXXXXXXXXXXXXXXX",
+        projectId: "XXXXXXXXXXXXXXXXX",
+        storageBucket: "XXXXXXXXXXXXXXXXX",
+        messagingSenderId: "XXXXXXXXXXXXXXXXX",
+        appId: "XXXXXXXXXXXXXXXXX",
+        measurementId: "XXXXXXXXXXXXXXXXX"
+    },
+    Google_Map_Key: "XXXXXXXXXXXXXXXXX",
+    facebookAppId:"XXXXXXXXXXXXXXXXX"
+}
