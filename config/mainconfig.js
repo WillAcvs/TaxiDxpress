807abd007cf7d727b848309cd67f7c2b21f15b9d
WillAcvs <desarrollointegralapp@gmail.com>
2021-03-10 14:39:54 -0700

    update

--- config/mainconfig.js
+++ config/mainconfig.js
@@ -1,21 +1,21 @@
 export const MainConfig = {
     AppDetails: {
-        app_name: "GrabCab", 
-        app_description: "GrabCab Taxi Booking App",
-        app_identifier: "com.exicube.grabcab",
+        app_name: "Dxpress", 
+        app_description: "Dxpress Taxi Booking App",
+        app_identifier: "com.dxpress.app",
         ios_app_version: "2.1", 
         android_app_version: 21
     },
     FirebaseConfig: {
-        apiKey: "XXXXXXXXXXXXXXXXX",
-        authDomain: "XXXXXXXXXXXXXXXXX",
-        databaseURL: "XXXXXXXXXXXXXXXXX",
-        projectId: "XXXXXXXXXXXXXXXXX",
-        storageBucket: "XXXXXXXXXXXXXXXXX",
-        messagingSenderId: "XXXXXXXXXXXXXXXXX",
-        appId: "XXXXXXXXXXXXXXXXX",
-        measurementId: "XXXXXXXXXXXXXXXXX"
+        apiKey: "AIzaSyCDXttQCBxpPYQV6GLzIlZnAHAuhICU4J8",
+    authDomain: "dxpress-v2.firebaseapp.com",
+    databaseURL: "https://dxpress-v2-default-rtdb.firebaseio.com",
+    projectId: "dxpress-v2",
+    storageBucket: "dxpress-v2.appspot.com",
+    messagingSenderId: "797885679321",
+    appId: "1:797885679321:web:d84f679dd84a44790960c8",
+    measurementId: "G-XGB160JC97"
     },
-    Google_Map_Key: "XXXXXXXXXXXXXXXXX",
+    Google_Map_Key: "AIzaSyClMGKgAPonyBhYynkbHJmrZbZxvg02aFM",
     facebookAppId:"XXXXXXXXXXXXXXXXX"
 }
